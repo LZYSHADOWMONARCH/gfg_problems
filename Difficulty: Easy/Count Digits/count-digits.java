67731d9040dad8ecea63c6a985d23ba56ba785f2
SUJAL GUPTA <sujalgupta841@gmail.com>
2025-12-28 17:24:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countDigits(int n) {
+        // code here
+        int count=0;
+        while(n>0)
+        {
+            int lastdigit=n%10;
+            count=count+1;
+            n=n/10;
+        }
+        return count;
+    }
+}
